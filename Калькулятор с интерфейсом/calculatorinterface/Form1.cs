73df27d5695e16b96037ca9cefa61a15eb191c32
vanyavasilev2901-collab <vanyavasilev2901@gmail.com>
2025-10-02 12:25:23 +0900

    Add files via upload

---
+++ Калькулятор с интерфейсом/calculatorinterface/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculatorinterface
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonClick(object sender, EventArgs e)
+        {
+            var currentButton = sender as Button;
+            textBox1.Text += currentButton.Text;
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            var d = new DataTable();
+            textBox1.Text = d.Compute(textBox1.Text, "").ToString();
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            var str = "";
+            for (int i = 0; i < textBox1.Text.Length - 1; i++)
+            {
+                str += textBox1.Text[i];
+            }
+            textBox1.Text = str;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+    }
+}
